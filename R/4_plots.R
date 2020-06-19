471e6142acea68789aae890463b0567dd1e4ec64
brunomc-eco <brunomc.eco@gmail.com>
2020-06-19 16:14:06 -0300

    p-value in plots

--- R/4_plots.R
+++ R/4_plots.R
@@ -27,12 +27,19 @@ cors_tmin <- data.frame(model = model, r = cors$tasmin_r) %>%
   arrange(desc(r)) %>%
   mutate(r3=round(r,3))
 
+#Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+p_prec <- c("***","***","***","***","***","***","***","***","***")
+p_tasmax <- c("***","***","***","***","***","***","*",".",".")
+p_tasmin <- c("***","***","***","***","***","***","***","***","***")
+
+
 png("./figs/4_correl_all_prec.png", res = 300, width = 2000, height = 1100)
 ggplot(data = cors_prec, aes(x = reorder(model, -r3), y = r3, width=.5)) +
   geom_bar(stat = "identity") +
   xlab("") +
   ylab("") +
   geom_text(aes(label=r3), vjust=-0.3, size=3.5) +
+  geom_text(aes(label=p_prec), vjust=1.6, size=5, color = "white") +
   theme_classic() +
   ggtitle("Precipitação")
 dev.off()
@@ -43,6 +50,7 @@ ggplot(data = cors_tmax, aes(x = reorder(model, -r3), y = r3, width=.5)) +
   xlab("") +
   ylab("") +
   geom_text(aes(label=r3), vjust=-0.3, size=3.5) +
+  geom_text(aes(label=p_tasmax), vjust=1.6, size=5, color = "white") +
   theme_classic() +
   ggtitle("Temperatura Máxima")
 dev.off()
@@ -53,6 +61,7 @@ ggplot(data = cors_tmin, aes(x = reorder(model, -r3), y = r3, width=.5)) +
   xlab("") +
   ylab("") +
   geom_text(aes(label=r3), vjust=-0.3, size=3.5) +
+  geom_text(aes(label=p_tasmin), vjust=1.6, size=5, color = "white") +
   theme_classic() +
   ggtitle("Temperatura Mínima")
 dev.off()
